7d6fa5bef552fb3822dfed7874020bc667e592b9
gianlutech <gianluca.ingrao95@gmail.com>
2022-01-10 16:02:07 +0100

    array mail

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
+console.log('js ok')
 /* Mail
 Crea un array di email;
 Chiedi all’utente la sua email.
 controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato in console sull’esito del controllo.
 */
+
+const mail = ["io@mail.it", "tu@mail.it", "egli@mail.it", "noi@mail.it", "voi@mail.it", "essi@mail.it",];
+
+console.table(mail);
